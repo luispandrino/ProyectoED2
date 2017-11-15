94dcd2378fad908468534447c3da131cfb65608b
luispandrino <landrinomenendez@gmail.com>
2017-11-15 11:14:15 -0600

    cambios no importantes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const mongo = require('mongodb').MongoClient;
 const client = require('socket.io').listen(4000).sockets;
 
+
 // Connect to mongo
 mongo.connect('mongodb://127.0.0.1/mongochat', function(err, db){
     if(err){
